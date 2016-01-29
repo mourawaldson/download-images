f1058418d107693fcb9912ec1dee00aa81f2e6a2
Waldson Moura <mourawaldson@gmail.com>
2016-01-28 23:37:14 -0500

    initial commit

---
+++ download.sh
@@ -0,0 +1,13 @@
+# while read p
+# do
+#   echo $p
+# done <source.txt
+
+pattern='<img id="img.*" src="(.*)"'
+while read line
+do
+  if [[ $line =~ $pattern ]]; then
+    url=${BASH_REMATCH[1]}
+    curl -O ${url/_thumb/}
+  fi
+done < source.txt
